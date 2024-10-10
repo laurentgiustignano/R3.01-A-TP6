b571f653cd26f5bc70bae273d3ae9fac65a63541
Laurent Giustignano <laurent.giustignano@free.fr>
2024-10-10 15:35:29 +0200

    Début R3.01-A-TP6

---
+++ assets/js/courbes.js
@@ -0,0 +1,33 @@
+
+const data = [
+  {
+    unite: 'UE1', moyenne:
+      {semestre1: 15, semestre2: 18},
+    color: 'DarkOrange'
+  },
+  {
+    unite: 'UE2', moyenne:
+      {semestre1: 11, semestre2: 17},
+    color: 'DeepPink'
+  },
+  {
+    unite: 'UE3', moyenne:
+      {semestre1: 13, semestre2: 8},
+    color: 'DodgerBlue'
+  },
+  {
+    unite: 'UE4', moyenne:
+      {semestre1: 16, semestre2: 12},
+    color: 'LightSeaGreen'
+  },
+  {
+    unite: 'UE5', moyenne:
+      {semestre1: 9, semestre2: 16},
+    color: 'MediumOrchid'
+  },
+  {
+    unite: 'UE6', moyenne:
+      {semestre1: 14, semestre2: 19},
+    color: 'DarkOliveGreen'
+  },
+];
